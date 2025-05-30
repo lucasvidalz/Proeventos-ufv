cddfb70d5362e5c18a22784a10d745aa02b81663
Diogo Pedroso <diogo.pedroso@edu.pbh.gov.br>
2025-05-30 11:21:11 -0300

    implantacao docker

--- Front/ProEventos-App/src/app/helpers/DateTimeFormat.pipe.ts
+++ Front/ProEventos-App/src/app/helpers/DateTimeFormat.pipe.ts
@@ -1,4 +1,4 @@
-import { Constants } from './../util/Constants';
+import { Constants } from './../util/constants';
 import { DatePipe } from '@angular/common';
 import { Pipe, PipeTransform } from '@angular/core';
 
